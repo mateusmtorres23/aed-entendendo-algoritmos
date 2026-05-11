fd9861f9dce1b7149822bbb09a00ca5df2b31a5b
Mateus Torres <mmtorres230726@gmail.com>
2026-05-10 21:02:26 -0300

    resposta de reverse array

--- src/reverse_array.py
+++ src/reverse_array.py
@@ -2,4 +2,6 @@ from src.my_array import MyArray
 
 
 def reverse_array(array: MyArray) -> MyArray:
-    raise NotImplementedError
+    for i in range(len(array)//2):
+        array[i], array[len(array) - i - 1] = array[len(array) - i - 1], array[i]
+    return array
