e6fc3c7e8cfb31d75fb595182e2a43e225785e76
Mateus Torres <mmtorres230726@gmail.com>
2026-05-11 20:53:08 -0300

    resolução Moonwalk

--- src/kth_to_last.py
+++ src/kth_to_last.py
@@ -2,4 +2,15 @@ from src.my_node import MyNode
 
 
 def kth_to_last(head: MyNode, k: int) -> int:
-    raise NotImplementedError
+    fast = head
+    slow = head
+
+    for _ in range(k):
+        if fast is None: return -1
+        fast = fast.next
+
+    while fast is not None:
+        fast = fast.next
+        slow = slow.next
+    
+    return slow.value if slow else -1
