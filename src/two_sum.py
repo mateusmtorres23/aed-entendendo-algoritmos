1e9d8ca86fa0694440293237f1dd483e0d0761cb
Mateus Torres <mmtorres230726@gmail.com>
2026-05-10 22:32:44 -0300

    resolução two sum

--- src/two_sum.py
+++ src/two_sum.py
@@ -2,4 +2,8 @@ from src.my_array import MyArray
 
 
 def two_sum(array: MyArray, target: int) -> tuple[int, int]:
-    raise NotImplementedError
+    for i in range(len(array)):
+        for j in range(len(array)):
+            if i != j and array[i]+array[j] == target:
+                return (i, j)
+    return (-1, -1)
