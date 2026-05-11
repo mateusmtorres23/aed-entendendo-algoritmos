f3afef9a2c384da5eaf00faa474ab8291cbc1c89
Mateus Torres <mmtorres230726@gmail.com>
2026-05-10 22:34:29 -0300

    resolução reverse linked list

--- src/reverse_linked_list.py
+++ src/reverse_linked_list.py
@@ -2,4 +2,16 @@ from src.my_node import MyNode
 
 
 def reverse_linked_list(head: MyNode) -> MyNode:
-    raise NotImplementedError
+    prev = None
+    current = head
+
+    while current is not None:
+        next_node = current.next
+
+        current.next = prev
+
+        prev = current
+        current = next_node
+    
+    return prev
+     
