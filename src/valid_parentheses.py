00a2b3f8b015ee9c16b16934dce433bc46bef7ea
Mateus Torres <mmtorres230726@gmail.com>
2026-05-10 22:34:01 -0300

    resolução valid parentheses

--- src/valid_parentheses.py
+++ src/valid_parentheses.py
@@ -1,5 +1,19 @@
-# from src.my_stack import MyStack
-
+from src.my_stack import MyStack
 
 def is_valid_parentheses(string: str) -> bool:
-    raise NotImplementedError
+    stack = MyStack()
+    for i in string:
+        if i in '([{':
+            stack.push(i)
+        elif i == ')' and stack.peek() == '(':
+            stack.pop()
+        elif i == ']' and stack.peek() == '[':
+            stack.pop()
+        elif i == '}' and stack.peek() == '{':
+            stack.pop()
+        else:
+            stack.push(i)
+
+    if stack.size() == 0:
+        return True
+    return False
