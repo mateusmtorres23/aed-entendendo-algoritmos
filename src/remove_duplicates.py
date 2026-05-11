96c6ffd8104b6126c25fcfe56fcd1dd15479f034
Mateus Torres <mmtorres230726@gmail.com>
2026-05-11 20:52:35 -0300

    resolução remove duplicates

--- src/remove_duplicates.py
+++ src/remove_duplicates.py
@@ -2,4 +2,21 @@ from src.my_node import MyNode
 
 
 def remove_duplicates(head: MyNode) -> MyNode:
-    raise NotImplementedError
+    current = head
+
+    while current is not None:
+        last_unique = current
+        check = current.next
+
+        while check is not None:
+            if check.value == current.value:
+                last_unique.next = check.next
+                check = check.next
+                continue
+            
+            last_unique = check
+            check = check.next
+
+        current = current.next
+
+    return head
