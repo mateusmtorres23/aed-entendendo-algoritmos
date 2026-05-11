7793e8d9fde0839f4445eb6f886a92f8895d45f5
Mateus Torres <mmtorres230726@gmail.com>
2026-05-11 20:52:12 -0300

    resolução has cycle

--- src/has_cycle.py
+++ src/has_cycle.py
@@ -2,4 +2,13 @@ from src.my_node import MyNode
 
 
 def has_cycle(head: MyNode) -> bool:
-    raise NotImplementedError
+    slow = head
+    fast = head
+
+    while fast is not None and fast.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow == fast:
+            return True
+    return False
